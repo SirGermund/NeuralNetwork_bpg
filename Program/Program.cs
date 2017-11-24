19799c2cf62591423b7abe096b7986062850633f
SirGermund <33968095+SirGermund@users.noreply.github.com>
2017-11-25 00:35:54 +0300

    Added main and class for multiplying matrices

---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NeuralMatrix
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Matrix a1 = new Matrix(3, 3);
+			Matrix b1 = new Matrix(3, 1);
+			Matrix.PrintMatrix(a1.matrix);
+			Console.WriteLine();
+			Matrix.PrintMatrix(b1.matrix);
+			Console.WriteLine();
+			double[,] res;
+			res = Matrix.Multiply(a1.matrix, b1.matrix);
+			Matrix.PrintMatrix(res);
+		}
+	}
+}
---
+++ Program/Simple_Linalgebra.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace NeuralMatrix
+{
+	class Matrix
+	{
+		public double[,] matrix;
+		int N, M;
+		public Matrix(int n, int m)
+		{
+			matrix = new double[n, m];
+			N = n;
+			M = m;
+			Randomize();
+		}
+		void Randomize()
+		{
+			Random rnd = new Random();
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < M; j++)
+				{
+					matrix[i, j] = rnd.Next(5);
+				}
+			}
+		}
+		public static double[,] Multiply(double[,] a, double[,] b)
+		{
+			double[,] res = new double[a.GetLength(0), b.GetLength(1)];
+			for (int i = 0; i < a.GetLength(0); i++)
+			{
+				for (int j = 0; j < b.GetLength(1); j++)
+				{
+					for (int k = 0; k < a.GetLength(1); k++)
+					{
+						res[i, j] += a[i, k] * b[k, j];
+					}
+				}
+			}
+			return res;
+		}
+
+		public static void PrintMatrix(double[,] a)
+		{
+			for (int i = 0; i < a.GetLength(0); i++)
+			{
+				for (int j = 0; j < a.GetLength(1); j++)
+				{
+					Console.Write("{0}\t", a[i, j]);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
