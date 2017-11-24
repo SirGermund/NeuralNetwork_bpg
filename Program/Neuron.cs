f7ea12a00be487f7101f84fcdb1fb8ad593acf3a
SirGermund <germund@Palermos-MacBook-Air.local>
2017-11-25 01:14:45 +0300

    Downloaded from Mac

--- Program/Neuron.cs
+++ Program/Neuron.cs
@@ -1,12 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace NeuralNetwork_bpg
-{
+namespace Classification
+{/// <summary> 
+ /// Нейрон: 2 входящих веса, выход 2 предыдущих нейронов, собственный вход/выход, собственная дельта, градиенты 2 входящих синапсов, дельты 2 входящих весов, ошибка, альфа, eps, идеальный выход. 
+ /// Все для расчета данных параметров 
+ /// </summary> 
 	class Neuron
 	{
+		public static int layerinput, layerhidden, layeroutput;
+		public Neuron()
+		{
+			w = new double[layerinput];
+			dw = new double[layerinput];
+			p = new double[layerinput];
+			grad = new double[layerinput];
+
+		}
+		/// <summary>
+		/// Нейрон выходного слоя.
+		/// </summary>
+		/// <param name="ideal">Ideal.</param>
+		public Neuron(double ideal)
+		{
+			outideal = ideal;
+			w = new double[layerhidden];
+			dw = new double[layerhidden];
+			p = new double[layerhidden];
+			grad = new double[layerhidden];
+		}
+		public double[] w, dw, grad;
+		public double[] p;
+		public double
+		input, output,
+		error, outideal = -1, delta;
+		public static double alpha = 0.1234, eps = 0.1;
+		private void CalcInput()
+		{
+			input = 0;
+			for (int i = 0; i < w.Length; i++)
+			{
+				input += w[i] * p[i];
+			}
+		}
+		private void CalcOutput()
+		{
+			output = 1 / (1 + Math.Exp(-input));
+		}
+		public void CalcDeltaOut()
+		{
+			delta = (outideal - output) * (1 - output) * output;
+		}
+		public void CalcDelta(Neuron[] next, int num)
+		{
+			delta = 0;
+			for (int i = 0; i < layeroutput; i++)
+			{
+				delta += next[i].w[num] * next[i].delta;
+			}
+			delta *= (1 - output) * output;
+		}
+		private void CalcGradient()
+		{
+			for (int i = 0; i < p.Length; i++)
+			{
+				grad[i] = p[i] * delta;
+			}
+		}
+		private void CalcDeltaWeight()
+		{
+			for (int i = 0; i < w.Length; i++)
+			{
+				dw[i] = eps * grad[i] + alpha * dw[i];
+				w[i] += dw[i];
+			}
+		}
+		public void CalcIO()
+		{
+			CalcInput();
+			CalcOutput();
+		}
+
+		public void ChangeWeight()
+		{
+			CalcGradient();
+			CalcDeltaWeight();
+		}
+		public void CalcError()
+		{
+			error = Math.Pow(outideal - output, 2);
+		}
+		public void Randomize(Random rnd)
+		{
+			for (int i = 0; i < layerinput; i++)
+			{
+				w[i] = rnd.Next(-3, 3) * (rnd.NextDouble() + 1);
+			}
+		}
 	}
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,20 +1,95 @@
 ﻿using System;
 
-namespace NeuralNetwork_bpg
+namespace Classification
 {
-	class Program
+	class MainClass
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Matrix a1 = new Matrix(3, 3);
-			Matrix b1 = new Matrix(3, 1);
-			Matrix.PrintMatrix(a1.matrix);
-			Console.WriteLine();
-			Matrix.PrintMatrix(b1.matrix);
-			Console.WriteLine();
-			double[,] res;
-			res = Matrix.Multiply(a1.matrix, b1.matrix);
-			Matrix.PrintMatrix(res);
+			Neuron.layerinput = 2;
+			Neuron.layerhidden = 16;
+			Neuron.layeroutput = 1;
+
+			Random rnd = new Random();
+
+			Neuron[] H = new Neuron[Neuron.layerhidden];
+			for (int i = 0; i < Neuron.layerhidden; i++)
+			{
+				H[i] = new Neuron();
+				H[i].Randomize(rnd);
+			}
+
+			Neuron[] Y = new Neuron[Neuron.layeroutput];
+			for (int i = 0; i < Neuron.layeroutput; i++)
+			{
+				Y[i] = new Neuron(1);
+				Y[i].Randomize(rnd);
+			}
+
+			for (int epoha = 0; epoha < 10000; epoha++)
+			{
+				for (int a = 0; a < 2; a++)
+				{
+					for (int b = 0; b < 2; b++)
+					{
+						Y[0].outideal = Bool2Int(a, b);
+						for (int i = 0; i < Neuron.layerhidden; i++)
+						{
+							H[i].p[0] = a;
+							H[i].p[1] = b;
+							H[i].CalcIO();
+						}
+						for (int i = 0; i < Neuron.layeroutput; i++)
+						{
+							Y[i].p[0] = H[i].output;
+							Y[i].p[1] = H[i].output;
+							Y[i].p[2] = H[i].output;
+							Y[i].CalcIO();
+							Y[i].CalcDeltaOut();
+							Y[i].ChangeWeight();
+						}
+						for (int i = 0; i < Neuron.layerhidden; i++)
+						{
+							H[i].CalcDelta(Y, i);
+							H[i].ChangeWeight();
+						}
+					}
+				}
+				for (int a = 0; a < 2; a++)
+				{
+					for (int b = 0; b < 2; b++)
+					{
+						Y[0].outideal = Bool2Int(a,b);
+						for (int i = 0; i < Neuron.layerhidden; i++)
+						{
+							H[i].p[0] = a;
+							H[i].p[1] = b;
+							H[i].CalcIO();
+						}
+						for (int i = 0; i < Neuron.layeroutput; i++)
+						{
+							Y[i].p[0] = H[i].output;
+							Y[i].p[1] = H[i].output;
+							Y[i].p[2] = H[i].output;
+							Y[i].CalcIO();
+							Y[i].CalcError();
+						}
+						Console.WriteLine("a={0}\tb={1}\tideal={4}\tansw={2:F5}\terr={3:F5}", a, b, Y[0].output,Y[0].error, Y[0].outideal);
+					}
+				}
+			}
+		}
+
+		static int Bool2Int(int a, int b)
+		{
+			bool abool, bbool, res;
+			if (a == 1) abool = true;
+			else abool = false;
+			if (b == 1) bbool = true;
+			else bbool = false;
+			res = abool & bbool;
+			if (res) return 1;
+			else return 0;
 		}
 	}
 }
